7849d0d4ba7837989cbacf6f04a106a7b4ab0bdd
JuanVillaverdeRodriguez <juanvillaverderodriguez@gmail.com>
2024-10-15 21:15:54 +0200

    La distorsion ya va genial

--- ocnet/Source/Model/Synth/Processors/Effects/DistortionProcessor.cpp
+++ ocnet/Source/Model/Synth/Processors/Effects/DistortionProcessor.cpp
@@ -10,7 +10,7 @@
 
 #include "DistortionProcessor.h"
 
-DistortionProcessor::DistortionProcessor(int id) : oversampler(2, 2, juce::dsp::Oversampling<float>::FilterType::filterHalfBandFIREquiripple, true)
+DistortionProcessor::DistortionProcessor(int id) : oversampler(2, 2, juce::dsp::Oversampling<float>::FilterType::filterHalfBandPolyphaseIIR, true)
 {
     setId(id);
 }
